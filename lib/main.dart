abff9b7601925e10e387842ea76cdd5d42656663
Ibrahim Elbaz <ibrahim.elbaz.1011@gmail.com>
2025-12-20 11:52:30 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class EducationalApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Anmka lms test',
+      title: 'STP',
       debugShowCheckedModeBanner: false,
 
       // RTL & Localization - Arabic only
